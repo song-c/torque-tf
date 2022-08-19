46b1ce15521e77ebe527b10959e9bbbd02b70b95
Chris Fox <chris.f@quali.com>
2022-08-18 21:47:40 -0700

    s3

--- Customer-TF/S3-Bucket/s3.tf
+++ Customer-TF/S3-Bucket/s3.tf
@@ -1,25 +1,23 @@
 variable "aws_region" {
-  type    = string
-  default = "us-west-2"
+    type    = string
+    default = "us-west-2"
 }
 
 resource "aws_s3_bucket" "bucket" {
-  bucket = "mybucket-test-1995"
-  force_destroy = true  
+    bucket = "my-bucket-test-1995"
+    force_destroy = true  
+    acl = "public-read-write"
 
-
-
- tags = {
+    tags = {
     Name        = "My bucket"
-  }
+    }
 }
 
 output "ssm_value" {
-  value = aws_s3_bucket.bucket.bucket
+    value = aws_s3_bucket.bucket.bucket
 }
 
 provider "aws" {
-  region  = var.aws_region
-
+    region  = var.aws_region
 }
 
