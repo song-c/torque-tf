c36c1e77b4c9235e7586a66496baea2e3e85f6f7
Chris Fox <chris.f@quali.com>
2022-09-09 10:01:10 -0700

    Fixing comments

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,5 +1,7 @@
 
 output "public_dns" {
   value = aws_instance.myapp.public #something not sure??
+
+  # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
 
