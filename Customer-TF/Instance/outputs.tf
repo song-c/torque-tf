4767c4d2fdeec11667bd862c73a7aeb0b6e71a0e
song-c <song.c@qualisystems.com>
2022-11-08 15:59:45 -0500

    Update outputs.tf

--- Customer-TF/Instance/outputs.tf
+++ Customer-TF/Instance/outputs.tf
@@ -1,6 +1,7 @@
 
 output "public_dns" {
-  value = aws_instance.myapp.public #something not sure??
+  #value = aws_instance.myapp.public #something not sure??
+  value = aws_instance.myapp.ami
 
   # hint: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
 }
