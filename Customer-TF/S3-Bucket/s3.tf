c6c4a70f60a058e4c59dd26f98502276a18eb023
song-c <song.c@quali.com>
2022-11-09 15:00:01 -0500

    bucket name

--- Customer-TF/S3-Bucket/s3.tf
+++ Customer-TF/S3-Bucket/s3.tf
@@ -5,7 +5,8 @@ variable "aws_region" {
 
 resource "aws_s3_bucket" "bucket" {
     #bucket = "my-bucket-test-1995"
-    bucket = "my-bucket-test-${formatdate("YYYYMMDDhhmmss",timestamp())}"
+    #bucket = "my-bucket-test-${formatdate("YYYYMMDDhhmmss",timestamp())}"
+    bucket = "my-bucket-test-{{ sandboxid | downcase }}"
     force_destroy = true  
     acl = "public-read-write"
 
